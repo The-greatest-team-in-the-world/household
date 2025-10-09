c1898c3b448d7fc9cefeeb9c4024247359fb2f5c
Martin B <martinjborjesson@gmail.com>
2025-10-09 15:46:58 +0200

    add component PieChart

---
+++ src/components/pie-chart.tsx
@@ -0,0 +1,79 @@
+import { mockdataAtom } from "@/providers/mockdata-provider";
+import { ChoreCompletion } from "@/types/chore-completion";
+import { useAtomValue } from "jotai";
+import { StyleSheet, Text, View } from "react-native";
+import PieChartRN, { Slice, SliceLabel } from "react-native-pie-chart";
+
+interface Props {
+  chores: ChoreCompletion[];
+  size: number;
+}
+
+/**
+* Renders a pie-achrt for the specified chore.
+* @param chores Cannot contain chores with non matching ChoreIds.
+* @param size Diameter of the pie-chart.
+*/
+export default function PieChart({ chores, size }: Props) {
+  if (!chores || chores.length === 0) return null;
+
+  if (!chores.every(c => c.choreId === chores[0].choreId)) {
+    console.warn("Tried to render an array containing different chores.");
+    return null;
+  };
+
+  const db = useAtomValue(mockdataAtom);
+
+  function getMemberAvatar(id: string) {
+    const user = db.householdMembers.find(m => m.userId === id)
+
+    if (!user) {
+      console.warn(`Cannot find user with id: ${id}.`);
+      return { color: "#000", emoji: "⚠️" };
+    }
+
+    return user.avatar;
+  };
+
+  function getEffortPerUser() {
+    const effortPerUser: Record<string, number> = {};
+
+    for (const chore of chores) {
+      if (!effortPerUser[chore.userId]) {
+        effortPerUser[chore.userId] = 0;
+      }
+
+      effortPerUser[chore.userId] += chore.choreEffort;
+    }
+
+    return effortPerUser;
+  }
+
+  const series: Slice[] = Object.entries(getEffortPerUser()).map(([userId, effort]) => {
+    const avatar = getMemberAvatar(userId);
+
+    const color = avatar.color
+    const value = effort;
+    const label: SliceLabel = { text: avatar.emoji }
+
+    return { color, value, label }
+  })
+
+  return (
+    <View style={s.container}>
+      <Text style={s.title}>{chores[0].choreName}</Text>
+      <PieChartRN widthAndHeight={size} series={series} />
+    </View>
+  )
+}
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    margin: 10,
+  },
+})
