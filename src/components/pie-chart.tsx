0e776b65e50e76595de4e803be3f18fcac317a13
Martin B <martinjborjesson@gmail.com>
2025-10-10 17:49:28 +0200

    make changes to PieChart to allow it to render total scores

--- src/components/pie-chart.tsx
+++ src/components/pie-chart.tsx
@@ -6,20 +6,27 @@ import PieChartRN, { Slice, SliceLabel } from "react-native-pie-chart";
 interface Props {
   chores: ChoreCompletion[];
   size: number;
+  total?: boolean;
 }
 
 /**
  * Renders a pie-achrt for the specified chore.
- * @param chores Cannot contain chores with non matching ChoreIds.
+ * @param chores May not contain chores with non matching ChoreIds without the use
+ * of `total` parameter.
  * @param size Diameter of the pie-chart.
+ * @param total If this parameter is used, the Piechart will ignore the check for
+ * multiple chores and instead render the total score for all chores with the title
+ * set in "totalTitle". 
  */
-export default function PieChart({ chores, size }: Props) {
+export default function PieChart({ chores, size, total }: Props) {
   if (!chores || chores.length === 0) return null;
 
-  if (!chores.every((c) => c.choreId === chores[0].choreId)) {
+  if (!chores.every((c) => c.choreId === chores[0].choreId) && !total) {
     console.warn("Tried to render an array containing different chores.");
     return null;
   }
+
+  const totalTitle = "Totalt";
 
   function getEffortPerUser() {
     const effortPerUser: Record<string, number> = {};
@@ -50,7 +57,9 @@ export default function PieChart({ chores, size }: Props) {
   return (
     <View style={s.container}>
       <PieChartRN widthAndHeight={size} series={series} />
-      <Text style={s.title}>{chores[0].choreName}</Text>
+      <Text style={s.title}>
+        {total ? totalTitle : chores[0].choreName}
+      </Text>
     </View>
   );
 }
@@ -61,6 +70,6 @@ const s = StyleSheet.create({
   },
   title: {
     margin: 10,
-    fontWeight: 600,
+    fontWeight: "600",
   },
 });
