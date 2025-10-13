842255232a0094767f01e9f225483ed4d0f381e3
Laxen <patrik.lax@live.se>
2025-10-13 15:03:21 +0200

    rename screen

--- src/app/(protected)/household-details.tsx
+++ src/app/(protected)/household-details.tsx
@@ -1,6 +1,6 @@
 import { View } from "react-native";
 
-export default function HouseHoldIndexScreen() {
+export default function HouseHoldDetailsScreen() {
   // Detaljsida för ett hushåll
   return <View></View>;
 }
