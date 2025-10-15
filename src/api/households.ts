2ce9b33ff2871e23ebacf7c743ca6cb7305722ea
Caroline Fagner <hcfagner@gmail.com>
2025-10-15 20:56:19 +0200

    get the right list

--- src/api/households.ts
+++ src/api/households.ts
@@ -1,14 +1,40 @@
 import { db } from "@/../firebase-config";
 import { Household } from "@/types/household";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { HouseholdMember } from "@/types/household-member";
+import {
+  collectionGroup,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
-export async function getUsersHouseholds(uid: string): Promise<Household[]> {
-  const householdsRef = collection(db, "households");
+export async function getUsersHouseholds(
+  uid: string,
+): Promise<(Household & { isOwner: boolean })[]> {
+  const snap = await getDocs(
+    query(collectionGroup(db, "members"), where("userId", "==", uid)),
+  );
 
-  const q = query(householdsRef, where("ownerIds", "array-contains", uid));
-  const snapshot = await getDocs(q);
+  if (snap.empty) return [];
 
-  return snapshot.docs.map(
-    (doc) => ({ id: doc.id, ...doc.data() }) as Household,
+  const households = await Promise.all(
+    snap.docs.map(async (mdoc) => {
+      const memberData = mdoc.data() as HouseholdMember;
+      const householdRef = mdoc.ref.parent.parent;
+      if (!householdRef) return null;
+      const hsnap = await getDoc(householdRef);
+      return hsnap.exists()
+        ? ({
+            id: hsnap.id,
+            ...hsnap.data(),
+            isOwner: memberData.isOwner,
+          } as Household & { isOwner: boolean })
+        : null;
+    }),
+  );
+
+  return households.filter(
+    (h): h is Household & { isOwner: boolean } => h !== null,
   );
 }
