6b4938ca0328d9f8c0da33ead356e48609edfc8e
Laxen <patrik.lax@live.se>
2025-10-16 20:23:24 +0200

    add chore details screen

--- src/app/(protected)/chore-details.tsx
+++ src/app/(protected)/chore-details.tsx
@@ -1,6 +1,46 @@
-import { View } from "react-native";
+import { selectedChoreAtom } from "@/atoms/chore-atom";
+import { currentHouseholdMember } from "@/atoms/member-atom";
+import { useAtomValue } from "jotai";
+import { StyleSheet } from "react-native";
+import { Divider, Surface, Text } from "react-native-paper";
 
 export default function ChoreDetailsScreen() {
-  // Detaljsida för chores
-  return <View></View>;
+  const selectedChore = useAtomValue(selectedChoreAtom);
+  const currentUser = useAtomValue(currentHouseholdMember);
+  console.log("is Owner:", currentUser?.isOwner);
+
+  return (
+    <Surface style={s.container} elevation={4}>
+      <Text style={s.choreName}>{selectedChore?.name}</Text>
+      <Divider />
+      <Text>Beskrivning</Text>
+      <Text style={s.choreDescription}>{selectedChore?.description}</Text>
+      <Text>{selectedChore?.frequency}</Text>
+      <Text>{selectedChore?.effort}</Text>
+      {currentUser?.isOwner && (
+        <Surface style={s.container} elevation={4}>
+          <Text>Jag är Owner</Text>
+        </Surface>
+      )}
+    </Surface>
+  );
 }
+
+const s = StyleSheet.create({
+  container: {
+    backgroundColor: "#e2e2e2ff",
+    margin: 16,
+    padding: 16,
+    borderRadius: 8,
+  },
+  choreName: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  choreDescription: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+});
