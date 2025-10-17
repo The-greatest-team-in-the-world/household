6864bbc3e85e1ce235c3155930f1540c96ffefc2
Martin B <martinjborjesson@gmail.com>
2025-10-17 15:46:50 +0200

    add properties

--- src/components/pie-chart.tsx
+++ src/components/pie-chart.tsx
@@ -1,37 +1,36 @@
-import { db } from "@/data/mock-db";
+import { Chore } from "@/types/chore";
 import { ChoreCompletion } from "@/types/chore-completion";
+import { HouseholdMember } from "@/types/household-member";
+import GetChoreName from "@/utils/get-chore-name";
 import getMemberAvatar from "@/utils/get-member-avatar";
 import { StyleSheet, Text, View } from "react-native";
 import PieChartRN, { Slice, SliceLabel } from "react-native-pie-chart";
 
 interface Props {
-  chores: ChoreCompletion[]; // array of completions for a single chore
+  completedChores: ChoreCompletion[];
+  chores: Chore[];
+  members: HouseholdMember[];
   size: number;
   iconSize?: number;
   titleSize?: number;
   total?: boolean;
 }
 
-/**
- * Renders a pie-achrt for the specified chore.
- * @param chores May not contain chores with non matching ChoreIds without the use
- * of `total` parameter.
- * @param size Diameter of the pie-chart. Used for widthAndHeight property of
- * Piechart component.
- * @param total If this parameter is used, the Piechart will ignore the check for
- * multiple chores and instead render the total score for all chores with the title
- * set in "totalTitle".
- */
 export default function PieChart({
+  completedChores,
   chores,
+  members,
   size,
   iconSize,
   titleSize,
   total,
 }: Props) {
-  if (!chores || chores.length === 0) return null;
+  if (!completedChores || completedChores.length === 0) return null;
 
-  if (!chores.every((c) => c.choreId === chores[0].choreId) && !total) {
+  if (
+    !completedChores.every((c) => c.choreId === completedChores[0].choreId) &&
+    !total
+  ) {
     console.warn("Tried to render an array containing different chores.");
     return null;
   }
@@ -41,11 +40,11 @@ export default function PieChart({
   function getEffortPerUser() {
     const effortPerUser: Record<string, number> = {};
 
-    const choreId = chores[0].choreId;
-    const chore = db.chores.find((c) => c.id === choreId);
+    const choreId = completedChores[0].choreId;
+    const chore = chores.find((c) => c.id === choreId);
     const perCompletionEffort = chore?.effort ?? 1;
 
-    for (const completion of chores) {
+    for (const completion of completedChores) {
       const memberId = completion.userId;
       if (!effortPerUser[memberId]) effortPerUser[memberId] = 0;
       effortPerUser[memberId] += perCompletionEffort;
@@ -56,7 +55,7 @@ export default function PieChart({
 
   const series: Slice[] = Object.entries(getEffortPerUser()).map(
     ([memberId, effort]) => {
-      const avatar = getMemberAvatar([], memberId);
+      const avatar = getMemberAvatar(members, memberId);
 
       const color = avatar.color;
       const value = effort;
@@ -72,9 +71,7 @@ export default function PieChart({
     <View style={s.container}>
       <PieChartRN widthAndHeight={size} series={series} />
       <Text style={[s.title, { fontSize: titleSize }]}>
-        {total
-          ? totalTitle
-          : (db.chores.find((c) => c.id === chores[0].choreId)?.name ?? "fel")}
+        {total ? totalTitle : GetChoreName(chores, completedChores[0].choreId)}
       </Text>
     </View>
   );
