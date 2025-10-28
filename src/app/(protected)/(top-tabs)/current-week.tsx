a3e1998963c825fac6d82806a8112b5d83e09634
Martin B <martinjborjesson@gmail.com>
2025-10-28 12:37:44 +0100

    style screen for when there is no completed chores this week

--- src/app/(protected)/(top-tabs)/current-week.tsx
+++ src/app/(protected)/(top-tabs)/current-week.tsx
@@ -6,7 +6,7 @@ import { ChoreCompletion } from "@/types/chore-completion";
 import { getChoresFromCurrentWeek } from "@/utils/get-statistics-from-range";
 import { useLocalSearchParams } from "expo-router";
 import { useAtomValue } from "jotai";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 
 export default function CurrentWeekStatisticsScreen() {
@@ -35,8 +35,16 @@ export default function CurrentWeekStatisticsScreen() {
   if (completions.length === 0) {
     return (
       <View style={s.noChoresContainer}>
-        <Text>Inga sysslor är registrerade för den här veckan ännu.</Text>
-        <Text>Börja med att lägga till eller markera en syssla som klar!</Text>
+        <Image
+          source={require("@/assets/images/not-found.png")}
+          style={s.image}
+        />
+        <Text style={[s.noChoresText, s.largeText]}>
+          Inga sysslor är registrerade för den här veckan ännu.
+        </Text>
+        <Text style={s.noChoresText}>
+          Börja med att lägga till eller markera en syssla som klar!
+        </Text>
       </View>
     );
   }
@@ -84,6 +92,19 @@ const s = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 50,
+    gap: 30,
+  },
+  image: {
+    maxWidth: "70%",
+    maxHeight: 300,
+    objectFit: "scale-down",
+  },
+  noChoresText: {
+    textAlign: "center",
+  },
+  largeText: {
+    fontSize: 20,
   },
   chartContainerTotal: {
     margin: 20,
