2386ebd2a0ed91247c3f7dbca2cd961ac186bb4c
Kruille <Krister>
2025-10-30 14:13:44 +0100

    Fix hiding lottie

--- src/app/(protected)/index.tsx
+++ src/app/(protected)/index.tsx
@@ -54,6 +54,7 @@ export default function HouseholdsScreen() {
 
       return Number(aInactive) - Number(bInactive);
     });
+
   const setVisible = useSetAtom(slideVisibleAtom);
   const setShouldRender = useSetAtom(shouldRenderSlideAtom);
   const initPendingListener = useSetAtom(initPendingMembersListenerAtom);
